43589fd8ec6998f043d965872a73035f311c91e3
Lim Jun Ying <Cyberpandaz@hotmail.com>
2019-03-26 14:02:21 +0800

    add gitignore

---
+++ src/main/java/action/MaryDetails.java
@@ -0,0 +1,24 @@
+package action;
+
+public class MaryDetails {
+    private double avgMonthlyExpenditure = ;
+    private double avgMonthlyIncome = ;
+    private double avgMonthlyFood = ;
+    private double avgMonthlyTransport = ;
+
+    public double getAvgMonthlyExpenditure() {
+        return avgMonthlyExpenditure;
+    }
+
+    public double getAvgMonthlyIncome() {
+        return avgMonthlyIncome;
+    }
+
+    public double getAvgMonthlyFood() {
+        return avgMonthlyFood;
+    }
+
+    public double getAvgMonthlyTransport() {
+        return avgMonthlyTransport;
+    }
+}
