bb4cfc92e645515bc770445ad726b06a633a3154
Lim Jun Ying <Cyberpandaz@hotmail.com>
2019-03-26 14:49:40 +0800

    2nd version

--- src/main/java/action/prediction.java
+++ src/main/java/action/prediction.java
@@ -1,18 +1,25 @@
 package action;
 
 public class prediction {
+
     private double averageSavings;
+    private double amount;
+    private int years;
 
     public prediction (double amount, int years) {
         this.amount = amount;
         this.years = years;
+
     }
 
-    public String getMessage ()
+    public String getMessage (){
+        return "";
+    }
 
     public String getPrediction (double amountFromUser, int yearsFromUser, String userName) {
         int timePeriod = yearsFromUser*12;
         double monthlySavings = amountFromUser/timePeriod;
         String message =
+                return "";
     }
 }
